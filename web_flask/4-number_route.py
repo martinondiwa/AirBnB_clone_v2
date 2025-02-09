75ca3352d9ff7baeaf79070b6d2512aa75a74ca2
martinondiwa <surekondiwa@gmail.com>
2025-02-09 16:42:19 +0000

    asg

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""script that strat web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hello_hbnb():
+    """dsiplay"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def hello_txt(text):
+    """display c ..."""
+    text = text.replace('_', ' ')
+    return ("C {}".format(text))
+
+
+@app.route('/python',  defaults={'text': "is cool"}, strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def hello_pyt(text):
+    """display python ..."""
+    txt = text.replace("_", " ")
+    return ("python {}".format(txt))
+
+
+@app.route('/number/<int:n>/', strict_slashes=False)
+def number(n):
+    """display number"""
+    if isinstance(n, int):
+        return ("{} is a number".format(n))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
